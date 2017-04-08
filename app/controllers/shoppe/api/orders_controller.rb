046b71ab4c045cf5ba5477e00a74d99fc5da5f19
Andres Marin <mb.andres@gmail.com>
2017-04-08 20:44:47 -0300

    Added order time support

--- app/controllers/shoppe/api/orders_controller.rb
+++ app/controllers/shoppe/api/orders_controller.rb
@@ -78,6 +78,9 @@ module Shoppe
         unless params[:payment_amount].nil?
           @order.properties[:payment_amount] = params[:payment_amount]
         end
+        unless params[:order_time].nil?
+          @order.properties[:order_time] = params[:order_time]
+        end
         @order.ip_address = params[:ip]
         @order.proceed_to_confirm
         @errors = JSON.parse(@order.errors.to_json)
--- lib/shoppe/version.rb
+++ lib/shoppe/version.rb
@@ -1,3 +1,3 @@
 module Shoppe
-  VERSION = '1.2.10'.freeze
+  VERSION = '1.2.11'.freeze
 end
