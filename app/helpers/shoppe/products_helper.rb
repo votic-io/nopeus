62659f97fa71d3a80657a120532ffdcfb0d12ab1
Andres Marin <mb.andres@gmail.com>
2017-07-16 14:16:01 -0300

    1.3.133 - Products API: Default scope except on find

--- app/helpers/shoppe/products_helper.rb
+++ app/helpers/shoppe/products_helper.rb
@@ -4,9 +4,9 @@ module Shoppe
 		def fetch_product id
 			if /\A\d+\z/.match(id)
       			id = id.to_i
-      			product =Shoppe::Product.find(id.to_i)
+      			product =Shoppe::Product.where(active: [true,false]).find(id.to_i)
       		else
-				product =Shoppe::Product.find_by_permalink(id)
+				product =Shoppe::Product.where(active: [true,false]).find_by_permalink(id)
 			end
 
 			return product
--- lib/shoppe/version.rb
+++ lib/shoppe/version.rb
@@ -1,3 +1,3 @@
 module Shoppe
-  VERSION = '1.3.132'.freeze
+  VERSION = '1.3.133'.freeze
 end
