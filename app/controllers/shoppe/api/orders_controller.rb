4cec76878431ed58d53d6b2fdf9a2c9266fca81c
manis2bess <unacosita.1@gmail.com>
2018-07-15 16:50:48 -0300

    1.3.180 - Orders Paging

--- app/controllers/shoppe/api/orders_controller.rb
+++ app/controllers/shoppe/api/orders_controller.rb
@@ -5,7 +5,7 @@ module Shoppe
       include Shoppe::ProductsHelper
 
       def index
-        @orders = Shoppe::Order.ordered.received.includes(order_items: :ordered_item)
+        @orders = Shoppe::Order.ordered.received.includes(order_items: :ordered_item).page(params[:page])
         render 'index'
       end
 
--- lib/shoppe/version.rb
+++ lib/shoppe/version.rb
@@ -1,3 +1,3 @@
 module Shoppe
-  VERSION = '1.3.179'.freeze
+  VERSION = '1.3.180'.freeze
 end
